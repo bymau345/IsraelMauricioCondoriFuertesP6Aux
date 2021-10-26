36cde3942d9a6e3590d723d67d403e1e105db737
israelMauricioCOndoriFuertes <condorimauricio6@gmail.com>
2021-10-25 23:02:30 -0400

    P6Aux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+  
+  const [movies, setMovies] = useState([]);
+  
+  const [content, setContent] = useState(0);
 
+  useEffect(() => {
+      getFilms();
+  }, [])
+
+  const getFilms = async () => {
+      const resp = await axios.get('https://swapi.dev/api/films')
+      setMovies(resp.data.results);
+  }
+  return (
+      <div>
+          <button type="button" onClick={()=>{setContent(content+1)}}>click</button>
+          {content < movies.length ? (<table class="">
+              <tr>
+                  <th>Titulo</th>
+                  <th>Descripcion</th>
+                  <th>Año</th>
+              </tr>
+              <tr key={movies.title}>
+                <td >{movies[content].title}</td>
+                <td >{movies[content].opening_crawl}</td>
+                <td >{movies[content].release_date}</td>
+              </tr>
+          
+          </table>) 
+          : ''}
+          
+      </div>
+        
+  )
+}
 export default App;
